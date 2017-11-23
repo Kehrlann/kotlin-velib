aa854c69745099ed2ec551698e23e48a3fa1ff16
Daniel Garnier-Moiroux <git@garnier.wf>
2017-11-23 08:37:32 +0100

    Initial commit : bare Spring initializr project

---
+++ src/main/kotlin/wf/garnier/velibtest/VelibTestApplication.kt
@@ -0,0 +1,11 @@
+package wf.garnier.velibtest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VelibTestApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(VelibTestApplication::class.java, *args)
+}
