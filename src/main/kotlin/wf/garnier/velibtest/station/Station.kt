dedfaac731b843cec43c06ff180a6aac8b703013
Daniel Garnier-Moiroux <git@garnier.wf>
2017-11-24 23:02:04 +0100

    Create default constructor for Station class

--- src/main/kotlin/wf/garnier/velibtest/station/Station.kt
+++ src/main/kotlin/wf/garnier/velibtest/station/Station.kt
@@ -4,4 +4,4 @@ import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
-data class Station(@Id val id: Long, val nom: String, val isOpen: Boolean = true, val isConnected: Boolean = true)
+data class Station(@Id val id: Long = 0L, val nom: String = "", val isOpen: Boolean = true, val isConnected: Boolean = true)
