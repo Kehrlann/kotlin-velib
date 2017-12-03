bb8addd0fee5fcd9509ccfa6a0ce66c50cf63421
Daniel Garnier-Moiroux <git@garnier.wf>
2017-12-03 19:07:22 +0100

    Added a simple test for our "StatusResponse" mapping

--- src/test/kotlin/wf/garnier/velibtest/status/StatusScrapperTest.kt
+++ src/test/kotlin/wf/garnier/velibtest/status/StatusScrapperTest.kt
@@ -1,5 +1,31 @@
 package wf.garnier.velibtest.status
 
-import org.junit.Assert.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import javax.xml.bind.JAXBContext
 
-class StatusScrapperTest
+
+class StatusScrapperTest {
+
+    private val velibDetail = javaClass.classLoader.getResourceAsStream("fixtures/velib-status-detail.xml")
+
+    @Test
+    fun `properly deserialize response`() {
+        val jc = JAXBContext.newInstance(StatusResponse::class.java)
+        val unmarshaller = jc.createUnmarshaller()
+
+        val result = unmarshaller.unmarshal(velibDetail) as StatusResponse
+
+        assertThat(result).isEqualTo(
+                StatusResponse(
+                        available = 5,
+                        free = 20,
+                        total = 25,
+                        ticket = 1,
+                        open = false,
+                        updated = 1507634830,
+                        connected = false
+                )
+        )
+    }
+}
