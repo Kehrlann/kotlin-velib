34f5852075a9a355fdf3e8e9863f549912da0c4c
Daniel Garnier <dgarnier@pivotal.io>
2017-12-08 18:45:08 +0100

    Added coroutine-based parallel scraping, 5 in parallel

--- src/main/kotlin/wf/garnier/velibtest/status/ScrappingScheduler.kt
+++ src/main/kotlin/wf/garnier/velibtest/status/ScrappingScheduler.kt
@@ -1,9 +1,14 @@
 package wf.garnier.velibtest.status
 
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.TextMessage
 import wf.garnier.velibtest.VelibConfiguration
+import wf.garnier.velibtest.station.Station
 import wf.garnier.velibtest.station.StationRepository
 
 @Component
@@ -14,31 +19,49 @@ class ScrappingScheduler(
         val config: VelibConfiguration
 ) {
     val logger = LoggerFactory.getLogger(ScrappingScheduler::class.java)
+    val scrappingQueue = Channel<Station>(10)
 
     fun startPolling() {
-        while (true) {
-            getStatusAndEmitAllStations()
+        runBlocking {
+            scrape()
+            while(true) {
+                scheduleScrapes()
+            }
+        }
+    }
+
+    suspend fun scheduleScrapes() {
+        val stations = repo.findAll()
+
+        stations.forEach {
+            scrappingQueue.send(it)
+            delay(config.sleepDurationBetweenApiCallsMilliseconds)
+        }
+    }
+
+    fun scrape() {
+        repeat(5) {
+            launch {
+                for (station in scrappingQueue) {
+                    scrapeAndEmit(station)
+                }
+            }
+        }
+    }
+
+    fun scrapeAndEmit(station: Station) {
+        try {
+            val status = scrapper.getVelibStatus(station.id).get().body
+            logger.debug("Got info for station with id : ${station.id}, emitting.")
+            emitMessage(StationStatus(station, status).toJson())
+        } catch (e: Exception) {
+            logger.error("Error polling stations with id : ${station.id}. Error : ${e.message}.")
         }
     }
 
     fun emitMessage(message: String) {
         state.currentSessions.forEach {
             it.sendMessage(TextMessage(message))
-        }
-    }
-
-    fun getStatusAndEmitAllStations() {
-        val stations = repo.findAll()
-
-        stations.forEach {
-            try {
-                val status = scrapper.getVelibStatus(it.id).get().body
-                logger.debug("Got info for station with id : ${it.id}, emitting.")
-                emitMessage(StationStatus(it, status).toJson())
-            } catch (e: Exception) {
-                logger.error("Error polling stations with id : ${it.id}. Error : ${e.message}.")
-            }
-            Thread.sleep(config.sleepDurationBetweenApiCallsMilliseconds)
         }
     }
 }
--- src/test/kotlin/wf/garnier/velibtest/status/ScrappingSchedulerTest.kt
+++ src/test/kotlin/wf/garnier/velibtest/status/ScrappingSchedulerTest.kt
@@ -1,10 +1,10 @@
 package wf.garnier.velibtest.status
 
+import kotlinx.coroutines.experimental.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.ArgumentCaptor
-import org.mockito.Mockito.anyLong
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.AsyncResult
@@ -15,8 +15,6 @@ import wf.garnier.velibtest.VelibConfiguration
 import wf.garnier.velibtest.station.Station
 import wf.garnier.velibtest.station.StationRepository
 import wf.garnier.velibtest.whenever
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
 
 class ScrappingSchedulerTest {
@@ -50,68 +48,68 @@ class ScrappingSchedulerTest {
     }
 
     @Test
-    fun `pollAndEmit should poll all available stations`() {
+    fun `scrapeStation should poll all available stations`() {
         val captors = setupReponsesAndCaptors()
 
         val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, config)
 
-        scrappingScheduler.getStatusAndEmitAllStations()
+        scrappingScheduler.scrapeAndEmit(stations.first())
 
-        assertThat(captors.scrapperCaptor.allValues).isEqualTo(stations.map { it.id })
+        assertThat(captors.scrapperCaptor.value).isEqualTo(stations.first().id)
     }
 
     @Test
-    fun `pollAndEmit should send message through websocket`() {
+    fun `scrapeStation should send message through websocket`() {
         val captors = setupReponsesAndCaptors()
 
         val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, config)
 
-        scrappingScheduler.getStatusAndEmitAllStations()
+        scrappingScheduler.scrapeAndEmit(stations.first())
 
-        val actualMessages = captors.websocketCaptor.allValues.map { it.payload }
-        assertThat(actualMessages).isEqualTo(statuses.map { it.toJson() })
+        assertThat(captors.websocketCaptor.value.payload).isEqualTo(statuses.first().toJson())
     }
 
     @Test
-    fun `pollAndEmit should not fail when HTTP fails`() {
-        val httpSuccess = AsyncResult(ResponseEntity(status, HttpStatus.OK))
-
-        // Return the first station twice
-        val captors = setupReponsesAndCaptors(listOf(stations.first(), stations.first()))
-
+    fun `scrapeStation should not fail when HTTP fails`() {
         whenever(mockScrapper.getVelibStatus(anyLong()))
                 .thenThrow(HttpClientErrorException(HttpStatus.I_AM_A_TEAPOT))
-                .thenReturn(httpSuccess)
 
         val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, config)
 
-        scrappingScheduler.getStatusAndEmitAllStations()
-
-        val actualMessages = captors.websocketCaptor.allValues.map { it.payload }
-        assertThat(actualMessages).isEqualTo(statuses.take(1).map { it.toJson() })
+        scrappingScheduler.scrapeAndEmit(stations.first())
     }
 
     @Test
-    fun `pollAndEmit sleeps during between calls`() {
-        // Arrange
-        val stationsToPoll = listOf(Station(), Station(), Station())
-        setupReponsesAndCaptors(stationsToPoll)
-        val millisBetweenPolls = 500L
+    fun `scrape relies on scrapeAndEmit`() {
+        val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, config)
+        val spy = spy(scrappingScheduler)
 
-        val startTime = LocalDateTime.now()
-        val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, VelibConfiguration(sleepDurationBetweenApiCallsMilliseconds = millisBetweenPolls))
+        spy.scrape()
 
-        // Act
-        scrappingScheduler.getStatusAndEmitAllStations()
+        runBlocking {
+            (1L..5L).forEach {
+                spy.scrappingQueue.send(Station(it))
+            }
+        }
 
-        // Assert
-        val timeSpan = startTime.until(LocalDateTime.now(), ChronoUnit.MILLIS)
-        assertThat(timeSpan).isGreaterThan(stationsToPoll.size * millisBetweenPolls) // LONGER than the min duration
-        assertThat(timeSpan).isLessThan((stationsToPoll.size + 1) * millisBetweenPolls) // But not too long.
-        // NB : the above is a bit flaky, it the total cost of ops of polls is greater than the "millisBetweenPolls",
-        // the test might fail. A cleaner way to test this could be to inject a "clock" object that then could be mocked.
+        fun anyStatus() = any() ?: Station()
+        verify(spy, times(5)).scrapeAndEmit(anyStatus())
     }
 
+    @Test
+    fun `scheduleScapes schedules the stations to be scrapped`() {
+        setupReponsesAndCaptors()
+        val scrappingScheduler = ScrappingScheduler(mockState, mockRepo, mockScrapper, config)
+        val received = mutableListOf<Station>()
+
+        runBlocking {
+            scrappingScheduler.scheduleScrapes()
+            stations.forEach {
+                received.add(scrappingScheduler.scrappingQueue.receive())
+            }
+        }
+        assertThat(received).isEqualTo(stations)
+    }
 
     private fun setupReponsesAndCaptors(
             stations: Collection<Station> = this.stations
