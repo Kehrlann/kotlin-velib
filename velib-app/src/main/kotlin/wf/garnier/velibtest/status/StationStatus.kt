ad2404c830f1c67b7654baf5ce3ca06e49adb1cf
Daniel Garnier-Moiroux <git@garnier.wf>
2017-12-10 23:16:54 +0100

    Replace Station.nom by Station.name

--- velib-app/src/main/kotlin/wf/garnier/velibtest/status/StationStatus.kt
+++ velib-app/src/main/kotlin/wf/garnier/velibtest/status/StationStatus.kt
@@ -6,7 +6,7 @@ import wf.garnier.velibtest.station.Station
 data class StationStatus(val id: Long, val name: String, val available: Int, val free: Int, val hasProblem: Boolean) {
 
     constructor(station: Station, status: StatusResponse):
-            this(station.id, station.nom, status.available, status.free, !status.connected || !status.open)
+            this(station.id, station.name, status.available, status.free, !status.connected || !status.open)
 
     fun toJson() = ObjectMapper().writeValueAsString(this)
 }
